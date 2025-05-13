51eb68c37d13f20803893bac22921302582f201e
Mariusz <mariusz@berriesinthebasket.com>
2025-05-13 12:57:26 +0200

    Initial Commit

---
+++ MoviesRecommendation/MoviesRecommendationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesRecommendationApp.swift
+//  MoviesRecommendation
+//
+//  Created by Mariusz Smoliński on 13.05.25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesRecommendationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
